f970dcdc45db57b06de47d3ac242731e1a01b243
Matthew McGee <mrmatthewmcgee@gmail.com>
2019-03-18 22:07:49 -0400

    final

--- server.js
+++ server.js
@@ -24,10 +24,33 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://mrmcgee:WaW920338@mattscraper-ri1se.mongodb.net/test?retryWrites=true", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
+});
+
+axios.get("http://books.toscrape.com").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    const $ = cheerio.load(response.data);
+    // console.log(response.data);
+    var result = {};
+
+    $(".product_pod").each(function (i, attr) {
+        
+        result.image = $(this)
+            .children(".image_container")
+            .children("a")
+            .children("img")
+            .attr("src")
+            console.log(result);
+        result.title
+    })
+    
 });
 
 module.exports = app;
